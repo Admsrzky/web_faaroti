b9fd3b2ce835647fa93bdc8f4ac34cf6a2e8b36c
Admsrzky <adimasrizki926@gmail.com>
2025-08-15 02:40:53 +0700

    gitaction

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,29 +1,23 @@
-#!/bin/bash
-set -e
+#!/bin/sh
+set -e # Keluar dari skrip jika ada perintah yang gagal
 
-echo "Deployment started ..."
+echo "Deployment dimulai..."
 
-# Turn ON Maintenance Mode or return true
-# if already is in maintenance mode
-(php artisan down) || true
+# 1. Aktifkan maintenance mode
+php artisan down || true
 
-# Pull the latest version of the app
+# 2. Ambil kode terbaru dari Git
 git pull origin main
 
-# Install composer dependencies
-composer install --optimize-autoloader --no-dev --no-interaction
+# 3. Install dependensi PHP
+composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 
-# Clearing Cache
-php artisan cache:clear
-php artisan config:clear
+# 5. Buat ulang file cache untuk optimasi
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
-# Recreate cache
-php artisan optimize
-
-# Run database migrations
-php artisan migrate --force
-
-# Turn OFF Maintenance mode
+# 6. Matikan maintenance mode
 php artisan up
 
-echo "Deployment finished!"
+echo "Deployment selesai!"
